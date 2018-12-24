a98533880d5a9fdfce1261fe1e75951af45f0ac0
pikeview <manas.marthi@gmail.com>
2018-12-24 19:35:14 +0000

    added course table to courses app

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
+'''
+   access db using sqlite3 db.sqlite3
+   sqlite> .tables
+   table names will  have appname_tablename format (courses_course)
+   sqlite> schema coures_course
+'''
